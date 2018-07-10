5106268968310e03565d423f86936bb4ffa8c3f2
Wladmyr Almeida <wladmyralmeida@gmail.com>
2018-07-10 19:57:33 -0300

    Projeto Criado

---
+++ src/main/java/br/com/prototipo/PrototipoTgApplication.java
@@ -0,0 +1,12 @@
+package br.com.prototipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipoTgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototipoTgApplication.class, args);
+	}
+}
